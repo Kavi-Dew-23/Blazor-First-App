58227866b9da0a894158380b8cf1025517eeacc0
student3 <student3@rinstrum.com>
2024-08-27 16:54:06 +0530

    Modify the dashboard and product pages

--- FirstApp.Frontend/Clients/ProductsClient.cs
+++ FirstApp.Frontend/Clients/ProductsClient.cs
@@ -10,7 +10,7 @@ public class ProductsClient
     new(){
         Id = 1,
         Name = "Test 1",
-        Published = "image",
+        Published = "Active",
         Price = 20.32M,
         ReleaseAt = new DateOnly(2024,07,3)
     },
@@ -18,7 +18,7 @@ public class ProductsClient
         Id = 2,
         
         Name = "Test 2",
-        Published = "image",
+        Published = "Draft",
         Price = 32.34M,
         ReleaseAt = new DateOnly(2024,07,04)
     },
@@ -26,10 +26,29 @@ public class ProductsClient
         Id = 3,
        
         Name = "Test 3",
-        Published = "image",
+        Published = "Draft",
         Price = 35.64M,
         ReleaseAt = new DateOnly(2024,07,13)
     },
     ];
+
+    private readonly Published[] publishes = new PublishesClient().GetPublishes();
     public ProductTable[] GetProducts() => [..products];
+
+    public void AddProduct(ProductDetails product)
+    {
+        // if the publishedId is null, always there ahould be a value for publiesed or not
+        ArgumentException.ThrowIfNullOrWhiteSpace(product.PublishedId);
+        var published = publishes.Single(published => published.Id == int.Parse(product.PublishedId));
+        var productTable = new ProductTable
+        {
+            Id = products.Count +1,
+            Name = product.Name,
+            Published = published.Name,
+            Price = product.Price,
+            ReleaseAt = product.ReleaseAt
+        };
+
+        products.Add(productTable);
+    }
 }
